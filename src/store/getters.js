eeb5057b838d81e6fac9b5f3f36854caa9644e41
Radicalfive <1561548288@qq.com>
2022-05-05 12:52:40 +0800

    feat: screenfull 全屏功能

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,4 +1,4 @@
-import variables from '@/styles/css/variables.module.scss'
+// import variables from '@/styles/css/variables.module.scss'
 import { MAIN_COLOR } from '@/constant'
 import { getItem } from '@/utils/storage'
 import { generateColors } from '@/utils/theme'
@@ -19,6 +19,7 @@ const getters = {
     }
   },
   sidebarOpened: (state) => state.app.sidebarOpened,
+  language: (state) => state.app.language,
   mainColor: state => state.theme.mainColor
 }
 export default getters
