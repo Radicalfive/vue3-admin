8a177e74efae4790dc80dfb2c390951150e8bdd0
Radicalfive <1561548288@qq.com>
2022-05-05 09:18:15 +0800

    feat: 左侧菜单伸缩功能实现

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="app-wrapper">
+  <div class="app-wrapper" :class="[$store.getters.sidebarOpened ? 'openSidebar' : 'hideSidebar']">
     <!-- 左侧 menu -->
     <sidebar class="sidebar-container" :style="{ backgroundColor: variables.menuBg }" />
     <div class="main-container">
@@ -38,5 +38,10 @@ import variables from '@/styles/css/variables.module.scss'
   right: 0;
   z-index: 9;
   width: calc(100% - #{$sideBarWidth});
+  transition: width #{$sideBarDuration};
+}
+
+.hideSidebar .fixed-header {
+  width: calc(100% - #{$hideSideBarWidth});
 }
 </style>
