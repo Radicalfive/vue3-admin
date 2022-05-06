58335bdfa68ae053f4ec8f1642addd3edf14d8db
Radicalfive <1561548288@qq.com>
2022-05-06 22:26:57 +0800

    feat: contextMenu 事件处理

--- src/layout/components/TagsView/ContextMenu.vue
+++ src/layout/components/TagsView/ContextMenu.vue
@@ -13,6 +13,8 @@
 </template>
 
 <script setup>
+import { useRouter } from 'vue-router'
+import { useStore } from 'vuex'
 const props = defineProps({
   index: {
     type: Number,
@@ -20,11 +22,26 @@ const props = defineProps({
   }
 })
 
-const onRefreshClick = () => {}
+const router = useRouter()
+const onRefreshClick = () => {
+  router.go(0)
+}
+// 关闭右侧
+const store = useStore()
+const onCloseRightClick = () => {
+  store.commit('app/removeTagsView', {
+    type: 'right',
+    index: props.index
+  })
+}
 
-const onCloseRightClick = () => {}
-
-const onCloseOtherClick = () => {}
+// 关闭其他
+const onCloseOtherClick = () => {
+  store.commit('app/removeTagsView', {
+    type: 'other',
+    index: props.index
+  })
+}
 </script>
 
 <style lang="scss" scoped>
