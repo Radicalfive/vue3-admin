e9d39284f2ddbf63a554f2d6b62e96f84ae2dff5
Radicalfive <1561548288@qq.com>
2022-05-08 23:12:34 +0800

    feat: 局部打印功能实现

--- src/main.js
+++ src/main.js
@@ -17,9 +17,12 @@ import i18n from '@/i18n'
 
 // filter
 import installFilter from '@/filter/index'
+// 打印相关
+import installDirective from '@/directives'
 
 const app = createApp(App)
 installElementPlus(app)
 installIcons(app)
 installFilter(app)
+installDirective(app)
 app.use(store).use(router).use(i18n).mount('#app')
